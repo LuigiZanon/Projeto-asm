ea907f09e3fd095c3ff618f120412d8afdcdd6b1
LuigiZanon <lugazabugre@outlook.com>
2023-11-21 19:10:46 -0300

    Update Projeto.asm

--- Projeto.asm
+++ Projeto.asm
@@ -18,7 +18,8 @@
             db 10,13,'[1]- CADASTRAR um aluno'
             db 10,13,'[2]- CORRIGIR um cadastro'
             db 10,13,'[3]- GERAR planilha'
-            db 10,13,'[4]- ENCERRA PROGRAMA $'
+            db 10,13,'[4]- EDITAR os pesos'
+            db 10,13,'[5]- ENCERRA PROGRAMA $'
 
     pula_linha  db 10,13,'$'                        ;pula uma linha do prompt
 
@@ -39,9 +40,13 @@
                                                                                 ;de 5 usuários
     edit_msg            db 10,13, 'O que voce deseja editar?'       ;menu de edição
                         db 10,13,'[1] Nota'                         
-                        db 10,13,'[2] Nome $'
+                        db 10,13,'[2] Nome'
+                        db 10,13,'$'
+
     edit_nome_msg       db 10,13,'Qual nome voce deseja editar? $'  ;mensagem para inserir o nome que será editado
+
     edit_nome_novo_msg  db 10,13,'Insira o nome novo: $'            ;nome editado
+
     edit_erro           db 'Nao ha cadastros! $'                    ;mensagem de erro caso não haja cadastros
 
     edit_provas db 'Digite qual prova voce gostariade editar'       ;menu da edição de notas
@@ -56,32 +61,26 @@
     peso_p2_msg db 10,13,'Peso da P2: $'
     peso_p3_msg db 10,13,'Peso da P3: $'
 
-    peso_p1 db 0
-    peso_p2 db 0
-    peso_p3 db 0
+    peso_p1 dw 0
+    peso_p2 dw 0
+    peso_p3 dw 0
 
     ent_dec macro 
     ;entrada decimal retornando o valor em bl
         mov ah,01
-        int 21h
-        sub al,30h
-        xor ah,ah
-        push ax
-        mov ax,10
-        mul bx
-        pop bx
-        add bl,al
-    endm
+        int 21h             ;recebe o digito
 
-    calc_media macro
-    ;calcula a media e retorna o valor em al
-        xor ax,ax
-        mov al,notas_p1[di]
-        add al,notas_p2[di]
-        add al,notas_p3[di]
-        xor bx,bx
-        mov bl,3
-        div bl
+        sub al,30h          ;transforma em algarismo
+
+        xor ah,ah           ;zera o registrador aH para que seja retirado o lixo e apenas vá para pilha o que está armazenado em ax
+        push ax
+
+        mov ax,10           ;guarda multiplicador em ax para fazer a operação
+
+        mul bx              ;bx =  (bx X 10) + bx
+        pop bx
+
+        add bl,al
     endm
 
 
@@ -91,30 +90,7 @@ main PROC
     mov ds,ax
     mov es,ax
 
-    mov ah,09
-    lea dx,menu_pesos
-    int 21h
-
-    @erro_peso:
-    lea dx,peso_p1_msg
-    int 21h
-
-    mov cx,3
-    xor bx,bx
-    @decimal:
-        ent_dec
-    loop @decimal
-
-    cmp bl,100
-    jg @erro_peso
-
-    mov peso_p1,bl
-
-    mov ah,09
-    lea dx,peso_p2_msg
-    int 21h
-    lea dx,peso_p3_msg
-    int 21h
+    call pesos
 
 @MENU:
     mov ah,09               ;printa mensagem do menu principal
@@ -137,6 +113,9 @@ main PROC
     je plani
 
     cmp al,'4'
+    je @pesos
+
+    cmp al,'5'
     je encerra_prog
     jmp @INVALID
 
@@ -151,7 +130,10 @@ main PROC
     plani:
         call print_nomes    ;função de imprimir planilha
         jmp @MENU
-
+    
+    @pesos:
+        call pesos
+        jmp @MENU
         
 encerra_prog:
     mov ah,4ch              ;encerra programa
@@ -183,6 +165,8 @@ cadastro PROC
     push bx                 ;salva regs na pilha
     push cx
 
+    call pulalinha
+
     cmp n_cad,5             ;se já houver 5 cadastros retorna erro
     jnge @cad
 
@@ -208,7 +192,7 @@ cadastro PROC
     @while:
         int 21h
         cmp al,13                                   ;al,13?
-        je fora                                     ;sim, pula para 'fora'
+        je @p1_insert                               ;sim, pula para 'fora'
 
         cmp al,08h              ;backspace? Se sim incrementa contador para desconsiderar o valor e decrementa bx para voltar no vetor
         jne diferente
@@ -222,12 +206,12 @@ cadastro PROC
         inc bx                  ;passa para o próximo elemento
     loop @while
 
-    @valor_inv:
+    @valor_inv_p1:
     mov ah,09
     lea dx,invalido             ;msg de valor inválido
     int 21h
 
-    fora:
+    @p1_insert:
     xor bx,bx
     mov di,n_cad                ;move o número de cadastros para di (que também seria o "índice" do novo cadastro)
 
@@ -242,10 +226,18 @@ cadastro PROC
     loop @for1
 
     cmp bl,10                   ;se o valor é maior que 10 o valor é inválido (volta para o começo do loop)
-    jg @valor_inv
+    jg @valor_inv_p1
     
     mov notas_p1[di],bl         ;salva o valor no vetor
 
+    jmp @p2_insert
+
+    @valor_inv_p2:
+    mov ah,09
+    lea dx,invalido             ;msg de valor inválido
+    int 21h
+
+    @p2_insert:
     mov ah,09
     lea dx, notas_p2_insert     ;msg de inserção
     int 21h
@@ -258,10 +250,18 @@ cadastro PROC
     loop @for2
     
     cmp bl,10                   ;se for maior do que 10 retorna o loop + msg erro
-    ja @valor_inv
+    ja @valor_inv_p2
 
     mov notas_p2[di],bl         ;salva valor no vetor
 
+    jmp @p3_insert
+
+    @valor_inv_p3:
+    mov ah,09
+    lea dx,invalido             ;msg de valor inválido
+    int 21h
+
+    @p3_insert:
     mov ah,09
     lea dx, notas_p3_insert     ;msg de inserção
     int 21h
@@ -274,12 +274,9 @@ cadastro PROC
     loop @for3
 
     cmp bl,10                   ;se valor maior que 10 = valor inválido (volta o loop)
-    jg @valor_inv
+    jg @valor_inv_p3
 
     mov notas_p3[di],bl         ;salva valor da nota no vetor
-
-    calc_media                  ;chama macro de cálculo da média
-    mov medias[di],al           ;salva no vetor das médias
 
     add n_cad,1                 ;por fim, já que o usuário foi cadastrado incrementa o n_cad que representa o número total de cadastros
     retorna:
@@ -298,6 +295,8 @@ editt PROC
 
     cmp n_cad, 0                ;se não tiver cadastros retorna erro
     jne @valida_edit_n
+
+    call pulalinha
 
     mov ah,09
     lea dx,edit_erro            ;msg de erro
@@ -492,9 +491,6 @@ edit_nota PROC
         mov notas_p3[di],bl ;salva valor no vetor
     
     @voltaProg:
-    calc_media
-    mov medias[di],al
-
     pop di
     pop cx                  ;retorna valores no registrador
     pop bx
@@ -507,40 +503,51 @@ print_nomes PROC
 ;printa todos os nomes cadastrados com 10,13
 
     push ax
-    push bx
+    push bx                 ;salva valores dos regs na pilha
     push cx
     push dx
 
-    call pulalinha
+    cmp n_cad,0             ;se nao tiver cadastros -> dá erro + volta
+    jne @tem_cad
 
     mov ah,09
-    lea dx,planilha_msg
+    lea dx,edit_erro        ;imprime mensagem de erro
     int 21h
 
-    mov cx,n_cad
+    jmp @return
+
+    @tem_cad:
+    call pulalinha          ;pula uma linha no prompt
 
     mov ah,09
-    lea dx, alunos
-    xor bx,bx
+    lea dx,planilha_msg     ;imprime o cabeçalho
+    int 21h
+
+    mov cx,n_cad            ;guarda o número de cadastros no contador
+
+    mov ah,09
+    lea dx, alunos          ;move o offset do nome de um aluno da linha x para dx
+    xor bx,bx               ;zera o índice que guia o cadastro em questão nos vetores de notas
 
     @for:
         call pulalinha
-        int 21h
-        add dx,30
+        int 21h             ;imprime um nome
+        add dx,30           ;vai para a próxima linha da matriz dos nomes dos alunos (vai para o próximo nome)
         push dx
         push cx
         push ax
 
-        call print_notas
+        call print_notas    ;chama a função para imprimir as notas
         
-        inc bx
+        inc bx              ;incrementa o bx para ir para o próximo cadastro
         pop ax
         pop cx
         pop dx
-    loop @for   
+    loop @for               ;dá loop até que todos cadastros tenham sido impressos
 
+    @return:
     pop dx
-    pop cx
+    pop cx                  ;retorna valores dos registradores
     pop bx
     pop ax
 
@@ -549,142 +556,258 @@ print_nomes ENDP
 
 print_notas proc
 
-        xor ax,ax
-        mov al,notas_p1[bx]
+        xor ax,ax           ;zera ax para garantir que não haja lixo em ah
+        mov al,notas_p1[bx] ;move para al a nota da p1 de um aluno x
 
-        call sai_dec
+        call sai_dec        ;função de saída decimal
 
-        mov dl,20h
+        mov dl,20h          ;imprime um ' ' (espaço)
         int 21h
 
-        xor ax,ax
-        mov al,notas_p2[bx]
+        xor ax,ax           ;zera ax para garantir que não haja lixo em ah
+        mov al,notas_p2[bx] ;move para al a nota da p2 de um aluno x
 
-        call sai_dec
+        call sai_dec        ;função de saída decimal
 
-        mov dl,20h
+        mov dl,20h          ;imprime um ' ' (espaço)
         int 21h
 
-        xor ax,ax
-        mov al,notas_p3[bx]
+        xor ax,ax           ;zera ax para garantir que não haja lixo em ah
+        mov al,notas_p3[bx] ;move para al a nota da p3 de um aluno x
 
-        call sai_dec
+        call sai_dec        ;função de saída decimal
 
-        mov dl,20h
+        mov dl,20h          ;imprime um ' ' (espaço)
         int 21h
 
-        xor ax,ax
-        mov al,medias[bx]
+        xor ax,ax           ;zera ax para garantir que não haja lixo em ah
+        call calc_medias
 
-        call sai_dec 
+        call sai_dec           ;imprime um ' ' (espaço)
 
         ret
 print_notas endp
 
+calc_medias proc
+;calulca a media do aluno contido no indice bx
+;retorna media em ax
+    
+    mov di,bx               ;move o endereçamento para di
+    xor dx,dx               ;zera dx por conta das operações aritméticas
+    mov al,notas_p1[di]     ;move a nota da p1 do aluno x para al para fazer a operação
+    mov bx,peso_p1          ;move o peso para bx por conta do MUL
+    mul bx                  ;multiplica a nota da p1 pelo peso da p1
+    push ax                 ;guarda o resultado na pilha
+
+    xor ax,ax               ;move o endereçamento para di
+    xor dx,dx               ;zera dx por conta das operações aritméticas
+    mov al,notas_p2[di]     ;move a nota da p2 do aluno x para al para fazer a operação
+    mov bx,peso_p2          ;move o peso para bx por conta do MUL
+    mul bx                  ;multiplica a nota da p2 pelo peso da p2
+    push ax                 ;guarda o resultado na pilha
+
+    xor ax,ax               ;move o endereçamento para di
+    xor dx,dx               ;zera dx por conta das operações aritméticas
+    mov al,notas_p3[di]     ;move a nota da p3 do aluno x para al para fazer a operação
+    mov bx,peso_p3          ;move o peso para bx por conta do MUL
+    mul bx                  ;multiplica a nota da p3 pelo peso da p3
+
+    pop bx                  ;devolve os resultados da pilha em bx e cx para efetuar a soma de todos os valores
+    pop cx
+    add ax,bx
+    add ax,cx
+    
+    mov bx,peso_p1
+    add bx,peso_p2          ;soma todos os pesos
+    add bx,peso_p3
+
+    xor dx,dx
+    div bx                  ;divide a soma de todas as notas multiplicadas pelos pesos pela soma de todos os pesos
+    
+    mov bx,di               ;restaura bx
+
+    ret
+calc_medias endp
+
 busca PROC
 ;procedimento que exibe a planilha com os dados
     push ax
-    push bx
+    push bx                 ;salva os valores dos registradores na pilha
     push cx
     
-    call pulalinha
+    call pulalinha          ;pula uma linha do prompt
 
     mov ah,09
-    lea dx,busca_msg
+    lea dx,busca_msg        ;msg de inserção do busca
     int 21h
 
-    mov indice_busc,0
-    xor bx,bx
-    mov cx,30
+    mov indice_busc,0       ;zera o índice busca pois pode conter índice de comparação anterior
+    xor bx,bx               ;zera bx (usado para guardar string de busca)
+    mov cx,30               ;contador de 30 (tamanho máximo do vetor)
     mov ah,01
+
 @busca_while:
-    int 21h
+    int 21h                 ;recebe caracter
     cmp al, 13
-    je @busc_sai
+    je @busc_sai            ;se for <enter>, sai
 
     cmp al,08h
-    jne @nBackspace
-    inc cx
+    jne @nBackspace         ;se for <backspace>, decrementa o registrador de endereçamento para 'voltar' uma casa + incrementa contador para descontabilizar valor
+    inc cx                      
     dec bx
     jmp @busca_while
 
     @nBackspace:
-    mov str_busca[bx],al
-    inc bx
+    mov str_busca[bx],al    ;armazena valor no vetor
+    inc bx                  ;passa para o próximo item
 
     loop @busca_while
 
 @busc_sai:
-    xor bx,bx
+    xor bx,bx               ;zera bx para utilizar no endereçamento da matriz de nomes
 
 @busc_cmp:
-    mov ax,30
+    mov ax,30               ;efetua a multiplicação do indice de busca (que no início é zero) pelo número de colunas para varrer a matriz
     mul indice_busc
 
-    mov bx,ax
+    mov bx,ax               ;guarda o resultado em ax
 
     cld
     mov cx,29                            ;cx com numero de caracteres
     mov dx, n_cad                        ;dx com o numero de cadastros
 
-    lea si, alunos[bx]
-    lea di, str_busca
+    lea si, alunos[bx]      ;offset do nome x em si
+    lea di, str_busca       ;offset do nome que está sendo buscado em str_busca
  
-    repe cmpsb
+    repe cmpsb              ;faz a comparação, se for igual -> sai
     jz @igual
 
-    inc indice_busc
+    inc indice_busc         ;caso não seja igual passa para a próxima linha
 
-    mov dx,n_cad
-    cmp indice_busc,dx
+    mov dx,n_cad            ;compara o indice de busca com o número total de cadastros e caso seja igual significa que ja varreu todos nomes
+    cmp indice_busc,dx      ;caso não tenha encontrado retorna mensagem de erro
     jne @busc_cmp
     
     lea dx, erro_busca
-    mov ah,09
+    mov ah,09               ;msg de erro do busca
     int 21h
 
-    xor dx,dx
+    xor dx,dx               ;zera o booleano (se 0-> não encontrou)
 
     jmp @sai_cmp
 
 @igual:
-    mov dx,1
+    mov dx,1                ;valor booleano que se for 1 demostra que encontrou um cadastro
 
 @sai_cmp:
-    xor bx,bx
-    mov cx,29
+    xor bx,bx               ;zera o contador bx para ser utilizado no endereçamento da limpeza da string de busca
+    mov cx,29               ;contador com o tamanho do vetor
     
 @zerabusca:
-    mov str_busca[bx],' '
+    mov str_busca[bx],' '   ;limpa vetor para poder ser utilizado novamente
     inc bx
 
     loop @zerabusca
 
     pop cx
-    pop bx
+    pop bx                  ;retorna valores aos registradores
     pop ax
 
     ret
 busca ENDP
+
 sai_dec proc
 ;sai decimal no dispositivo padrão
 ;al deve conter o valor que sera impresso
 
-    mov si,10
-    mov cx,2
+    mov si,10               ;já que a saída é decimal, move 10 para o divisor
+    mov cx,2                ;contador 2 pois o número tem 2 dígitos
+
     @loop1:
-        xor dx,dx
-        div si
-        push dx
+        xor dx,dx           ;zera o registrador do resto
+
+        div si              ;realiza a divisão
+        push dx             ;guarda o resto na pilha
+
     loop @loop1
+
     mov ah,02
-    mov cx,2
+    mov cx,2                ;como tem 2 dígitos seta o contador para 2
+
     @print:
-        pop dx
+        pop dx              ;puxa o resto da pilha e imprime como caracter
+        
         add dl,30h
         int 21h
     loop @print
 
     ret
 sai_dec endp
+
+pesos PROC
+;procedimento para inserir os respectivos pesos das provas(p1,p2,p3)
+
+    push ax
+    push bx                 ;salva valores dos registradores
+    push cx
+
+    mov ah,09
+    lea dx,menu_pesos       ;msg dos pesos
+    int 21h
+
+    @erro_peso:
+    mov ah,09
+    lea dx,peso_p1_msg      ;msg de inserção do peso da p1
+    int 21h
+
+    mov cx,3                ;move 3 para cx pois o número pode conter 3 dígitos
+    xor bx,bx               ;zera bx para armazenar os pesos
+
+    @decimal:
+        ent_dec             ;entrada decimal
+    loop @decimal
+
+    cmp bl,100              ;se o valor for maior que 100 volta o loop
+    jg @erro_peso
+
+    mov peso_p1,bx          ;armazena o valor na variável para peso da p1
+
+    mov ah,09
+    lea dx,peso_p2_msg      ;msg de inserção do peso da p2
+    int 21h
+
+    mov cx,3                ;move 3 para cx pois o número pode conter 3 dígitos
+    xor bx,bx               ;zera bx para armazenar os pesos
+
+    @decimal2:
+        ent_dec             ;entrada decimal
+    loop @decimal2
+
+    cmp bl,100              ;se o valor for maior que 100 volta o loop
+    jg @erro_peso
+    
+    mov peso_p2,bx          ;armazena o valor na variável para peso da p2
+
+    mov ah,09
+    lea dx,peso_p3_msg      ;msg de inserção do peso da p3
+    int 21h
+
+    mov cx,3                ;move 3 para cx pois o número pode conter 3 dígitos
+    xor bx,bx               ;zera bx para armazenar os pesos
+
+    @decimal3:
+        ent_dec             ;entrada decimal
+    loop @decimal3
+
+    cmp bl,100              ;se o valor for maior que 100 volta o loop
+    jg @erro_peso
+
+    mov peso_p3,bx          ;armazena o valor na variável para peso da p3
+
+    pop cx
+    pop bx                  ;retorna os valores dos registradores
+    pop ax
+
+    ret
+pesos ENDP
 end main
